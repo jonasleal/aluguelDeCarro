f6de3d2f47d298de0c69a9234d6e2f1d7f63cd62
Jonas Leal <jonaslabinfo@gmail.com>
2018-10-31 15:08:41 -0300

    Classes de negocio

--- src/br/ufrpe/aluguelDeCarro/negocio/ClienteNegocio.java
+++ src/br/ufrpe/aluguelDeCarro/negocio/ClienteNegocio.java
@@ -1,13 +1,75 @@
 package br.ufrpe.aluguelDeCarro.negocio;
 
+import br.ufrpe.aluguelDeCarro.dados.entidades.Cliente;
+import br.ufrpe.aluguelDeCarro.dados.repositorios.ClienteRepositorio;
+import br.ufrpe.aluguelDeCarro.dados.repositorios.interfaces.ClienteRepositorioInterface;
+import br.ufrpe.aluguelDeCarro.excecoes.CpfException;
+import br.ufrpe.aluguelDeCarro.excecoes.IdadeInvalidaExcetion;
+
 /**
  * @author Fernando
  */
 public class ClienteNegocio {
 
-    public static boolean verificarStringData(String data){
-        if(data != null)
+    private final ClienteRepositorioInterface repositorio;
+    
+    public ClienteNegocio(){
+        this.repositorio = new ClienteRepositorio();
+    }
+
+    public static boolean verificarStringData(String data) {
+        if (data != null) {
             return data.matches("\\d{4}-\\d{2}-\\d{2}");
+        }
         return false;
     }
+
+    private boolean validar(Cliente cliente) throws CpfException, IdadeInvalidaExcetion {
+        return cliente != null && cliente.valirdar();
+    }
+
+    public boolean cadastrar(Cliente cliente) throws CpfException, IdadeInvalidaExcetion {
+        boolean saida = false;
+        if (validar(cliente)) {
+            repositorio.cadastrar(cliente);
+            saida = true;
+        }
+
+        return saida;
+    }
+
+    public boolean alterar(Cliente cliente) throws CpfException, IdadeInvalidaExcetion {
+        boolean saida = false;
+        if (validar(cliente)) {
+            repositorio.alterar(cliente);
+            saida = true;
+        }
+        return saida;
+    }
+
+    public Cliente buscarPorId(int id) {
+        Cliente saida = null;
+        if (id > 0) {
+            saida = this.repositorio.buscarPorId(id);
+        }
+        return saida;
+    }
+
+    public Cliente buscarPorCpf(String cpf) {
+        Cliente saida = null;
+        if (cpf != null && !cpf.isEmpty()) {
+            saida = this.repositorio.buscarPorCpf(cpf);
+        }
+        return saida;
+    }
+
+    public boolean desativar(int id) {
+        boolean saida = false;
+        if (id > 0) {
+            repositorio.deletar(id);
+            saida = true;
+        }
+        return saida;
+    }
+
 }
---
+++ src/br/ufrpe/aluguelDeCarro/negocio/GerenteNegocio.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufrpe.aluguelDeCarro.negocio;
+
+import br.ufrpe.aluguelDeCarro.dados.entidades.Gerente;
+import br.ufrpe.aluguelDeCarro.dados.repositorios.GerenteRepositorio;
+import br.ufrpe.aluguelDeCarro.dados.repositorios.interfaces.GerenteRepositorioInterface;
+import br.ufrpe.aluguelDeCarro.excecoes.CpfException;
+import br.ufrpe.aluguelDeCarro.excecoes.IdadeInvalidaExcetion;
+
+/**
+ *
+ * @author JonasJr
+ */
+public class GerenteNegocio {
+
+    private final GerenteRepositorioInterface repositorio;
+    
+    public GerenteNegocio(){
+        this.repositorio = new GerenteRepositorio();
+    }
+    
+    private boolean validar(Gerente gerente) throws CpfException, IdadeInvalidaExcetion{
+        return gerente != null && gerente.validar();
+    }
+
+    public boolean cadastrar(Gerente gerente) throws CpfException, IdadeInvalidaExcetion {
+        boolean saida = false;
+        if(validar(gerente)){
+            repositorio.cadastrar(gerente);
+            saida = true;
+        }
+        return saida;
+    }
+    
+    public boolean alterar(Gerente gerente) throws CpfException, IdadeInvalidaExcetion{
+        boolean saida = false;
+        if(validar(gerente)){
+            this.repositorio.alterar(gerente);
+            saida = true;
+        }
+        return saida;
+    }
+    
+    public Gerente buscarPorId(int id){
+        Gerente saida = null;
+        if(id > 0){
+            saida = repositorio.buscarPorId(id);
+        }
+        return saida;
+    }
+    public Gerente buscarPorCpr(String cpf){
+        Gerente saida = null;
+        if(cpf != null && !cpf.isEmpty()){
+            saida = buscarPorCpr(cpf);
+        }
+        return saida;
+    }
+    
+    public boolean desativar(int id){
+        boolean saida = false;
+        if(id > 0){
+            repositorio.deletar(id);
+            saida = true;
+        }
+        return saida;
+    }
+    
+}
