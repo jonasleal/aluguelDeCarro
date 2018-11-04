f2759627d85c0c4d71cc380c0f68a1c65d9bacd2
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2018-11-03 22:40:58 -0300

    Adiciona metodo para transformar em localdatetime

--- src/br/ufrpe/aluguelDeCarro/servicos/DataUtil.java
+++ src/br/ufrpe/aluguelDeCarro/servicos/DataUtil.java
@@ -1,14 +1,23 @@
 package br.ufrpe.aluguelDeCarro.servicos;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Fernando
  */
 public class DataUtil {
+    public static LocalDateTime transformarStringEmDataTime(String string){
+        if(string != null && string.trim().length() > 0){
+            return LocalDateTime.parse(string, DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
+        }
+        return null;
+    }
+
     public static LocalDate transformarStringEmData(String string){
         if(string != null && string.trim().length() > 0){
-            return LocalDate.parse(string);
+            return LocalDate.parse(string, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         }
         return null;
     }
